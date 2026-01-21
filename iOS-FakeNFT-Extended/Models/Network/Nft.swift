ad829c989f2ae83afdf4240084ab2e108d55bbf0
Dmitry <lightpaintru@gmail.com>
2026-01-21 14:18:47 +0300

    [Feat] Расширил модель Nft и добавил модели корзины
    - Расширил модель Nft полями из API (name, rating, price и др.).
    - Добавил модели BasketItem, BasketSortOption и Currency для функционала корзины и оплаты

--- iOS-FakeNFT-Extended/Models/Network/Nft.swift
+++ iOS-FakeNFT-Extended/Models/Network/Nft.swift
@@ -1,6 +1,42 @@
+//
+//  Nft.swift
+//  NFT Market
+//
+//  Created by Dmitry on 21.01.2026.
+//
+
 import Foundation
 
-struct Nft: Decodable {
+struct Nft: Decodable, Identifiable, Sendable {
     let id: String
+    let name: String
     let images: [URL]
+    let rating: Int
+    let description: String
+    let price: Double
+    let author: String
+    let website: URL
+    let createdAt: Date
+}
+
+/// Модель записи NFT в корзине
+struct BasketItem: Identifiable, Sendable {
+    let id: String
+    let nft: Nft
+    let quantity: Int
+}
+
+/// Варианты сортировки корзины
+enum BasketSortOption: String, CaseIterable, Sendable {
+    case name
+    case price
+    case rating
+}
+
+/// Модель валюты для экрана оплаты
+struct Currency: Identifiable, Sendable {
+    let id: String
+    let name: String
+    let ticker: String
+    let imageURL: URL
 }
