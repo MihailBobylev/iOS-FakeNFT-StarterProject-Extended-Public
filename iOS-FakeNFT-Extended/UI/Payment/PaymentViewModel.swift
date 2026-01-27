18d70ab659545038b48b4961c7d6545da3d94ad5
Dmitry <lightpaintru@gmail.com>
2026-01-27 18:24:03 +0300

    [Feat] Создал PaymentViewModel для экрана оплаты

---
+++ iOS-FakeNFT-Extended/UI/Payment/PaymentViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  PaymentViewModel.swift
+//  NFT Market
+//
+//  Created by Dmitry on 27.01.2026.
+//
+
+import Foundation
+
+@Observable
+@MainActor
+final class PaymentViewModel {
+    
+    var currencies: [Currency]
+    var selectedCurrencyID: String?
+    
+    init(currencies: [Currency] = []) {
+        self.currencies = currencies
+    }
+    
+    var selectedCurrency: Currency? {
+        guard let id = selectedCurrencyID else { return nil }
+        return currencies.first { $0.id == id }
+    }
+}
+
