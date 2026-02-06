3187012b54907809b71c29024d489bad8c7fea39
Dmitry <lightpaintru@gmail.com>
2026-02-06 17:27:02 +0300

    [Fix] убрал токен

--- iOS-FakeNFT-Extended/Services/Requests/RequestConstants.swift
+++ iOS-FakeNFT-Extended/Services/Requests/RequestConstants.swift
@@ -1,4 +1,4 @@
 enum RequestConstants {
     static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
-    static let token = "a320271e-e28d-4f2f-a4ab-d0574081ea74"
+    static let token = ""
 }
